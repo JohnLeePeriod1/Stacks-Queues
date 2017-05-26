0ed4124c4f2bd04ce4eab1cbc2f7d1bc93415135
Brett Cockerill <cockerilla@bsd405.org>
2017-05-25 18:19:26 -0700

    Create Queue.java

---
+++ src/callCenter/Queue.java
@@ -0,0 +1,11 @@
+//*******************************************************
+// DO NOT MODIFY THIS FILE!!!
+//*******************************************************
+
+package callCenter;
+
+public interface Queue
+{
+	public void handleTick();
+	public Boolean allCustomersHandled();
+}

2a84bf94b6379440f0a2268fba91fb47682c71f4
Brett Cockerill <cockerilla@bsd405.org>
2017-05-25 19:01:19 -0700

    Update Tests.java

--- test/callCenterTests/Tests.java
+++ test/callCenterTests/Tests.java
@@ -1,6 +1,7 @@
 //*******************************************************
 //DO NOT MODIFY THIS FILE!!!
 //*******************************************************
+
 package callCenterTests;
 
 import java.io.ByteArrayInputStream;
@@ -21,17 +22,76 @@ import callCenter.CallCenter;
 public class Tests
 {
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
     
+	/**
+	 * Set the Standard output and error streams to
+	 * send to our private streams instead of the
+	 * console before each test runs
+	 */
 	@Before
-    public void setup() {
-            System.setOut(new PrintStream(outContent));
-            System.setErr(new PrintStream(errContent));
-    }
+	public void setup()
+	{
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+	}
+	
+	/**
+	 * Reset the Standard output, error, and input
+	 * streams back to their default (Console and//*******************************************************
+//DO NOT MODIFY THIS FILE!!!
+//*******************************************************
+
+package callCenterTests;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import callCenter.CallCenter;
+
+public class Tests
+{
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    
+	/**
+	 * Set the Standard output and error streams to
+	 * send to our private streams instead of the
+	 * console before each test runs
+	 */
+	@Before
+	public void setup()
+	{
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+	}
+	
+	/**
+	 * Reset the Standard output, error, and input
+	 * streams back to their default (Console and
+	 * System.in) after each test runs
+	 */
+	@After
+	public void cleanUpStreams()
+	{
+		System.setOut(null);
+		System.setErr(null);
+		System.setIn(System.in);
+	}
 	
 	@Test
-	public void testSample() {
-		
+	public void testSample()
+	{	
 		File expected = new File("sampleOutput.txt");
 		String inputString = "sampleInput.txt\n";
 		ByteArrayInputStream input = new ByteArrayInputStream(inputString.getBytes());
@@ -46,12 +106,65 @@ public class Tests
 		verifyOutput(expected);
 	}
 	
+	private String getExpectedString(File expectedFile)
+	{
+		String expected = "";
+		try {
+			Scanner file = new Scanner(expectedFile);
+			
+			while(file.hasNextLine())
+			{
+				expected += file.nextLine();
+				expected += "\r\n";
+			}			
+			
+			file.close();
+		}
+		catch (FileNotFoundException e) {
+			fail("Caught a FileNotFoundException");
+		}
+		
+		return expected;
+	}
+	
+	private void verifyOutput(File expectedFile)
+	{		
+		String actual = outContent.toString();
+		String expected = getExpectedString(expectedFile);
+			
+		if(!actual.equals(expected))
+		{
+			fail("Output doesn't match the expected!");
+		}
+	}
+}
+
+	 * System.in) after each test runs
+	 */
 	@After
-    public void cleanUpStreams() {
-        System.setOut(null);
-        System.setErr(null);
-        System.setIn(System.in);
-    }
+	public void cleanUpStreams()
+	{
+		System.setOut(null);
+		System.setErr(null);
+		System.setIn(System.in);
+	}
+	
+	@Test
+	public void testSample()
+	{	
+		File expected = new File("sampleOutput.txt");
+		String inputString = "sampleInput.txt\n";
+		ByteArrayInputStream input = new ByteArrayInputStream(inputString.getBytes());
+		System.setIn(input);
+		
+		try {
+			CallCenter.main(null);
+		} catch (FileNotFoundException e) {
+			fail("Caught a FileNotFoundException");
+		}
+		
+		verifyOutput(expected);
+	}
 	
 	private String getExpectedString(File expectedFile)
 	{

49d916d82814bd6576df502b345a978f74fb5432
Brett Cockerill <cockerilla@bsd405.org>
2017-06-11 13:59:41 -0700

    Create CallCenter.java

--- src/callCenter/CallCenter.java
+++ src/callCenter/CallCenter.java
@@ -7,7 +7,5 @@ public class CallCenter
 	public static void main(String[] args) throws FileNotFoundException
 	{
 		// TODO: Fill in with your code
-		
-		// Testing a small change to test travis and automatic unit tests
 	}
 }
